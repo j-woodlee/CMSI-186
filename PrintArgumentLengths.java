bd2aca8502f78538928e415ad55c5c93d6fff70b
j-woodlee <jwoodlee@lion.lmu.edu>
2015-01-24 14:48:56 -0800

    New condition in case zero arguments are entered

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,5 +1,10 @@
 public class PrintArgumentLengths{
         public static void main(String[] args){
+                if (args.length == 0) {
+			System.out.println("Usage: java PrintArgumentLengths <any number of arguments>");
+			return;
+		}
+		
                 for (String arg: args) {
                         System.out.println("arg: " + arg + ", length: " + arg.length());
                 }
