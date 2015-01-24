1f4859df63c103fa1fe7971d1cd329bfaeaa3b56
j-woodlee <jwoodlee@lion.lmu.edu>
2015-01-24 14:46:30 -0800

    Now prints the length of each argument

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,7 +1,7 @@
 public class PrintArgumentLengths{
         public static void main(String[] args){
                 for (String arg: args) {
-                        
+                        System.out.println("arg: " + arg + ", length: " + arg.length());
                 }
         }
 }
