fe59f353dde3f6975863c3f3fe8f436f156478bf
j-woodlee <jwoodlee@lion.lmu.edu>
2015-03-03 16:13:34 -0800

    Finished Ball Methods

--- Ball.java
+++ Ball.java
@@ -1,17 +1,16 @@
 public class Ball {
 
     // Declare my instance variables!
-    double radius;
-    Vector location;
-    Vector initialVelocity;
+    private double radius;
+    private Vector location;
+    private Vector velocity;
 
     
     public Ball(double radius, Vector location, Vector initialVelocity) {
         // Implement me!
         this.radius = radius;
         this.location = location;
-        this.initialVelocity = initialVelocity;
-
+        velocity = initialVelocity;
     }
 
     public Vector getLocation() {
@@ -23,14 +22,19 @@ public class Ball {
         // Implement me!
         return radius;
     }
+
+    public Vector getVelocity(){
+        return velocity;
+    }
     
     public void accelerate(Vector acceleration, double grain) {
-        // Implement me!
-        
+        //<0,-9.8>
+        this.velocity = this.velocity.add(acceleration.scale(grain));
     }
 
     public void move(double grain) {
         // Implement me!
+        this.location = this.location.add(this.velocity.scale(grain));
     }
 
 }
