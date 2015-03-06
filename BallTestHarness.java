3761f307bc45a2e4265af9bd68a05b3fc9da54b9
j-woodlee <jwoodlee@lion.lmu.edu>
2015-03-05 16:02:59 -0800

    Destruction of tabs

--- BallTestHarness.java
+++ BallTestHarness.java
@@ -1,18 +1,19 @@
 public class BallTestHarness {
-	private static int successes = 0;
-	private static int attempts = 0;
+    private static int successes = 0;
+    private static int attempts = 0;
 
-	public static void main(String[] args){
-		attempts = 0;
+    public static void main(String[] args){
+        attempts = 0;
         successes = 0;
 
         testConstructor();
         testAccelerate();
+        testMove();
 
         System.out.println(successes + "/" + attempts + " tests passed.");
-	}
+    }
 
-	private static void displaySuccessIfTrue(boolean value) {//courtesy of Dondi
+    private static void displaySuccessIfTrue(boolean value) {//courtesy of Dondi
         attempts++;
         successes += value ? 1 : 0;
 
@@ -24,14 +25,14 @@ public class BallTestHarness {
     }
 
     public static void testConstructor(){
-    	System.out.println("Testing Balls's Constructor...");
+        System.out.println("Testing Balls's Constructor...");
         double radius = 10.5;
-    	Vector location = new Vector(0,0);
-    	Vector velocity = new Vector(50,69);
+        Vector location = new Vector(0,0);
+        Vector velocity = new Vector(50,69);
 
-		Ball b = new Ball(radius,location,velocity);
+        Ball b = new Ball(radius,location,velocity);
 
-		try {
+        try {
             displaySuccessIfTrue(b.getRadius() == radius && b.getLocation().equals(location) && b.getVelocity().equals(velocity));
         } catch(Exception exc) {
             displayFailure();
@@ -89,7 +90,6 @@ public class BallTestHarness {
 
         Ball b = new Ball(10, new Vector(0,0), new Vector(10,10));
         b.accelerate(new Vector(0,-9.8), 0.5);
-
         System.out.println(b.getVelocity());
 
         try {
@@ -117,5 +117,84 @@ public class BallTestHarness {
         } catch(Exception exc) {
             displayFailure();
         }
+
+        b = new Ball(10, new Vector(0,0), new Vector(32,23));
+        b.accelerate(new Vector(0,-9.5),0);
+        System.out.println(b.getVelocity());
+
+        try {
+            displaySuccessIfTrue(b.getVelocity().equals(new Vector(32,23)));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        b = new Ball(10, new Vector(0,0), new Vector(32,-23));
+        b.accelerate(new Vector(0,-5),3);
+        System.out.println(b.getVelocity());
+
+        try {
+            displaySuccessIfTrue(b.getVelocity().equals(new Vector(32,-23-15)));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        b = new Ball(10, new Vector(0,0), new Vector(32,23));
+        b.accelerate(new Vector(-5,-5),0.3);
+        System.out.println(b.getVelocity());
+
+        try {
+            displaySuccessIfTrue(b.getVelocity().equals(new Vector(32 + (-5*.3),23 + (-5*.3))));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+
+    }
+
+    public static void testMove(){
+        System.out.println("Testing Move...");
+
+        Ball b = new Ball(10, new Vector(0,0), new Vector(10,10));
+
+        b.move(4.5);
+        System.out.println(b.getLocation());
+
+        try {
+            displaySuccessIfTrue(b.getLocation().equals(new Vector(45,45)));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        b = new Ball(10, new Vector(0,0), new Vector(10,10));
+
+        b.move(1);
+        System.out.println(b.getLocation());
+
+        try {
+            displaySuccessIfTrue(b.getLocation().equals(new Vector(10,10)));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        b = new Ball(10, new Vector(-34,-56), new Vector(4.5,7.6));
+        b.move(1);
+        System.out.println(b.getLocation());
+
+        try {
+            displaySuccessIfTrue(b.getLocation().equals(new Vector(-34 + 4.5, -56 + 7.6)));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        b = new Ball(10, new Vector(-34,-56), new Vector(4.5,7.6));
+        b.move(2.5);
+        System.out.println(b.getLocation());
+
+        try {
+            displaySuccessIfTrue(b.getLocation().equals(new Vector(-34 + 2.5*4.5,-56 + 2.5*7.6)));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
     }
 }
