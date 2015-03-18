994e8ff1a6b2ff19ecf19b8153a477449840a003
j-woodlee <jwoodlee@lion.lmu.edu>
2015-03-17 17:39:49 -0700

    added a new instance variable: stoppped

--- Ball.java
+++ Ball.java
@@ -4,13 +4,15 @@ public class Ball {
     private double radius;
     private Vector location;
     private Vector velocity;
+    private boolean stopped;
 
     
     public Ball(double radius, Vector location, Vector initialVelocity) {
         // Implement me!
         this.radius = radius;
         this.location = location;
-        velocity = initialVelocity;
+        this.velocity = initialVelocity;
+        this.stopped = false;
     }
 
     public Vector getLocation() {
@@ -27,6 +29,10 @@ public class Ball {
         return velocity;
     }
 
+    public void stop(){
+        this.stopped = true;
+    }
+
     public double getDistance(Ball b){//returns distance between this and b from the center
         return Math.sqrt(Math.pow(this.location.x() - b.getLocation().x(),2) + Math.pow(this.location.y() - b.getLocation().y(),2));
     }
@@ -38,7 +44,7 @@ public class Ball {
 
     public void move(double grain) {
         // Implement me!
-        this.location = this.location.add(this.velocity.scale(grain));
+        this.location = this.stopped ? this.location : this.location.add(this.velocity.scale(grain));
     }
 
     public boolean isTouching(Ball b){//return true if this is within or touching the radius of b
