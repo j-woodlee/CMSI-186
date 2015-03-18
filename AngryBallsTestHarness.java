07d227dfa49548d312895221f44c0be8d3d82aff
j-woodlee <jwoodlee@lion.lmu.edu>
2015-03-17 17:38:57 -0700

    Now works with all Classes

--- AngryBallsTestHarness.java
+++ AngryBallsTestHarness.java
@@ -1,7 +1,23 @@
 public class AngryBallsTestHarness {
+    static int attempts = 0;
+    static int successes = 0;
 
     public static void main(String[] args){
         VectorTestHarness.main(args);
         BallTestHarness.main(args);
+        AngryBallsSimulationTestHarness.main(args);
+
+        attempts = VectorTestHarness.attempts + BallTestHarness.attempts + AngryBallsSimulationTestHarness.attempts;
+        successes = VectorTestHarness.successes + BallTestHarness.successes + AngryBallsSimulationTestHarness.successes;
+
+        System.out.println(successes + "/" + attempts + " Total tests passed.");
+
+        if(VectorTestHarness.successes == VectorTestHarness.attempts && BallTestHarness.successes == BallTestHarness.attempts && AngryBallsSimulationTestHarness.successes == AngryBallsSimulationTestHarness.attempts) {
+            System.out.println("All Tests passed.");
+
+        } else {
+            System.out.println("At least one test has failed.");
+        }
+
     }
 }
