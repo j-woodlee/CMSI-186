6b694df79a2d87f2157de939bd90e483e68aae50
j-woodlee <jwoodlee@lion.lmu.edu>
2015-03-17 17:40:32 -0700

    Added more tests

--- BallTestHarness.java
+++ BallTestHarness.java
@@ -1,6 +1,6 @@
 public class BallTestHarness {
-    private static int successes = 0;
-    private static int attempts = 0;
+    static int successes = 0;
+    static int attempts = 0;
 
     public static void main(String[] args){
         attempts = 0;
@@ -9,6 +9,9 @@ public class BallTestHarness {
         testConstructor();
         testAccelerate();
         testMove();
+        testGetDistance();
+        testIsTouching();
+        
 
         System.out.println(successes + "/" + attempts + " tests passed.");
     }
@@ -81,7 +84,6 @@ public class BallTestHarness {
         } catch(Exception exc) {
             displayFailure();
         }
-
     }
 
     public static void testAccelerate(){
@@ -90,7 +92,6 @@ public class BallTestHarness {
 
         Ball b = new Ball(10, new Vector(0,0), new Vector(10,10));
         b.accelerate(new Vector(0,-9.8), 0.5);
-        System.out.println(b.getVelocity());
 
         try {
             displaySuccessIfTrue(b.getVelocity().equals(new Vector(10,5.1)));
@@ -100,7 +101,6 @@ public class BallTestHarness {
 
         b = new Ball(10, new Vector(0,0), new Vector(10,10));
         b.accelerate(new Vector(0,-9.5),1);
-        System.out.println(b.getVelocity());
 
         try {
             displaySuccessIfTrue(b.getVelocity().equals(new Vector(10,0.5)));
@@ -110,7 +110,6 @@ public class BallTestHarness {
 
         b = new Ball(10, new Vector(0,0), new Vector(10,10));
         b.accelerate(new Vector(0,-9.5),1);
-        System.out.println(b.getVelocity());
 
         try {
             displaySuccessIfTrue(b.getVelocity().equals(new Vector(10,0.5)));
@@ -120,7 +119,6 @@ public class BallTestHarness {
 
         b = new Ball(10, new Vector(0,0), new Vector(32,23));
         b.accelerate(new Vector(0,-9.5),0);
-        System.out.println(b.getVelocity());
 
         try {
             displaySuccessIfTrue(b.getVelocity().equals(new Vector(32,23)));
@@ -130,7 +128,6 @@ public class BallTestHarness {
 
         b = new Ball(10, new Vector(0,0), new Vector(32,-23));
         b.accelerate(new Vector(0,-5),3);
-        System.out.println(b.getVelocity());
 
         try {
             displaySuccessIfTrue(b.getVelocity().equals(new Vector(32,-23-15)));
@@ -140,7 +137,6 @@ public class BallTestHarness {
 
         b = new Ball(10, new Vector(0,0), new Vector(32,23));
         b.accelerate(new Vector(-5,-5),0.3);
-        System.out.println(b.getVelocity());
 
         try {
             displaySuccessIfTrue(b.getVelocity().equals(new Vector(32 + (-5*.3),23 + (-5*.3))));
@@ -157,7 +153,6 @@ public class BallTestHarness {
         Ball b = new Ball(10, new Vector(0,0), new Vector(10,10));
 
         b.move(4.5);
-        System.out.println(b.getLocation());
 
         try {
             displaySuccessIfTrue(b.getLocation().equals(new Vector(45,45)));
@@ -168,7 +163,6 @@ public class BallTestHarness {
         b = new Ball(10, new Vector(0,0), new Vector(10,10));
 
         b.move(1);
-        System.out.println(b.getLocation());
 
         try {
             displaySuccessIfTrue(b.getLocation().equals(new Vector(10,10)));
@@ -178,7 +172,6 @@ public class BallTestHarness {
 
         b = new Ball(10, new Vector(-34,-56), new Vector(4.5,7.6));
         b.move(1);
-        System.out.println(b.getLocation());
 
         try {
             displaySuccessIfTrue(b.getLocation().equals(new Vector(-34 + 4.5, -56 + 7.6)));
@@ -188,7 +181,6 @@ public class BallTestHarness {
 
         b = new Ball(10, new Vector(-34,-56), new Vector(4.5,7.6));
         b.move(2.5);
-        System.out.println(b.getLocation());
 
         try {
             displaySuccessIfTrue(b.getLocation().equals(new Vector(-34 + 2.5*4.5,-56 + 2.5*7.6)));
@@ -196,5 +188,93 @@ public class BallTestHarness {
             displayFailure();
         }
 
+        b = new Ball(10, new Vector(99999,3), new Vector(478787.5,78787978.6));
+        b.move(1);
+
+        try {
+            displaySuccessIfTrue(b.getLocation().equals(new Vector(578786.5 ,78787981.6)));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+    }
+    public static void testGetDistance(){
+        System.out.println("Testing getDistance...");
+        Ball ball1 = new Ball(10, new Vector(0,0), new Vector(10,10)); 
+        Ball ball2 = new Ball(10, new Vector(0,0), new Vector(10,10));
+
+        try {
+            displaySuccessIfTrue(ball1.getDistance(ball2) == 0);
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        ball1 = new Ball(10, new Vector(10,0), new Vector(10,10));
+
+        try {
+            displaySuccessIfTrue(ball1.getDistance(ball2) == 10);
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        ball2  = new Ball(10, new Vector(0,10), new Vector(10,10));
+
+        try {
+            displaySuccessIfTrue(ball1.getDistance(ball2) == Math.sqrt(200));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        ball1 = new Ball(10, new Vector(0,100), new Vector(10,10)); 
+        ball2 = new Ball(10, new Vector(-100,0), new Vector(10,10));
+
+        try {
+            displaySuccessIfTrue(ball1.getDistance(ball2) == Math.sqrt(20000));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+    }
+
+    public static void testIsTouching() {
+        System.out.println("Testing isTouching...");
+        Ball ball1 = new Ball(10, new Vector(0,0), new Vector(10,10)); 
+        Ball ball2 = new Ball(10, new Vector(0,0), new Vector(10,10));
+
+        try {
+            displaySuccessIfTrue(ball1.isTouching(ball2));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        ball1.move(1);
+
+        try {
+            displaySuccessIfTrue(ball1.isTouching(ball2));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        ball2.move(1);
+
+        try {
+            displaySuccessIfTrue(ball1.isTouching(ball2));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        ball1.move(1);
+
+        try {
+            displaySuccessIfTrue(ball1.isTouching(ball2));
+        } catch(Exception exc) {
+            displayFailure();
+        }
+
+        ball1.move(1);
+
+        try {
+            displaySuccessIfTrue(!ball1.isTouching(ball2));
+        } catch(Exception exc) {
+            displayFailure();
+        }
     }
 }
