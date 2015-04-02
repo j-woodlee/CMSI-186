daa922aac01f0af6c08776c992a9a60bdb1ec776
j-woodlee <jwoodlee@lion.lmu.edu>
2015-04-02 12:23:53 -0700

    BigFib working with BigInt

--- BigFib.java
+++ BigFib.java
@@ -1,22 +1,21 @@
-import java.math.BigInteger;
 
 public class BigFib {
     public static void main(String[] args){
 
-        int iterations = 1000;
+        int iterations = 100;
 
-        BigInteger num1 = new BigInteger("0");
-        BigInteger num2 = new BigInteger("1");
-        BigInteger num3 = new BigInteger("0");
+        BigInt num1 = new BigInt("0");
+        BigInt num2 = new BigInt("1");
+        BigInt num3 = new BigInt("0");
         
         while (iterations > 1){//0, 1, 1, 2, 3, 5, 8, 13
             System.out.println(num3);
 
-            if(num3.equals(new BigInteger("1"))){
+            if(num3.equals(new BigInt("1"))){
                 System.out.println(num3);
             }
 
-            num3 = num1.add(num2);
+            num3 = num1.plus(num2);
 
             num1 = num2;
             num2 = num3;
