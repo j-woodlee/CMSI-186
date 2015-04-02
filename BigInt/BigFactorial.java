b3dee8b0365011a3116088e39456a87a252fe8d7
j-woodlee <jwoodlee@lion.lmu.edu>
2015-04-02 12:43:49 -0700

    Calculates Factorials of BigInts

---
+++ BigInt/BigFactorial.java
@@ -0,0 +1,17 @@
+public class BigFactorial {
+    public static void main(String[] args) {
+        BigInt argument = new BigInt(args[0]);
+        BigInt factorial = new BigInt(args[0]);
+
+        
+        BigInt one = new BigInt("1");
+
+        while(argument.isGreaterThan(one)) {
+            System.out.println(argument);
+            argument = argument.minus(one);
+            factorial = factorial.times(argument);
+        }
+
+        System.out.println(factorial);
+    }
+}
