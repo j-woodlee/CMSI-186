72236e3d288a1ddd6a7054b3f35eb3b191762f26
j-woodlee <jwoodlee@lion.lmu.edu>
2015-04-07 15:27:50 -0700

    Update PiEstimator.java

--- PiEstimator.java
+++ PiEstimator.java
@@ -1,45 +1,40 @@
 public class PiEstimator {
-    private double x;
-    private double y;
+    private Dart d;
+    private int numDarts;
 
-    public PiEstimator() {
-        this.x = Math.random() * (Math.random() > .5 ? +1 : -1);
-        this.y = Math.random() * (Math.random() > .5 ? +1 : -1);//-1 and 1
+    public PiEstimator(int numDarts) {
+        d = new Dart();
+        this.numDarts = numDarts;
     }
 
     public static void main(String[] args) {
-        System.out.println("start");
-        int numDarts = 10000;
-
+        PiEstimator p = new PiEstimator(10000);
         if(args.length > 0) {
-            numDarts = Integer.parseInt(args[0]);
+            p = new PiEstimator(Integer.parseInt(args[0]));
         }
 
-        PiEstimator p;
+        System.out.println("start");
         int hits = 0;
-        
-        for(int i = 0; i < numDarts; i++) {
-            p = new PiEstimator();
-            System.out.print(p.x + " ");
-            System.out.print(p.y + " ");
 
-            if(p.onBoard()) {
-                System.out.print("in");
+
+        for(int i = 0; i < p.numDarts; i++) {
+            p.d = new Dart();
+
+            System.out.println(p.throwDart() + (p.d.onBoard() ? "in" : "out"));
+            if(p.d.onBoard()) {
                 hits++;
-            } else {
-                System.out.print("out");
             }
-            System.out.println(Math.pow(p.x,2) + Math.pow(p.y,2));
         }
-        System.out.println("end");
-        System.out.println(numDarts);
-        
 
-        System.out.println("Darts: " + numDarts + " Hits: " + hits + " Pi estimate: " + (double)(4*(double)hits)/(double)numDarts);
+        System.out.println("end");
+
+        System.out.println("Darts: " + p.numDarts + " Hits: " + hits + " Pi estimate: " + (double)(4*(double)hits)/(double)p.numDarts);
         System.out.println(hits + " darts thrown within the circle.");
+
+
     }
 
-    public boolean onBoard() {
-        return Math.pow(this.x,2) + Math.pow(this.y,2) <= 1;
+    public String throwDart() {
+        return d.x() + " " + d.y() + " ";
     }
 }
