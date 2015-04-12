663ac2d76f7e9d7f543d71f1bcc0e3aeeb977d9e
j-woodlee <jwoodlee@lion.lmu.edu>
2015-04-11 17:26:55 -0700

    Update PiEstimator.java

--- PiEstimator.java
+++ PiEstimator.java
@@ -9,17 +9,21 @@ public class PiEstimator {
 
     public static void main(String[] args) {
         PiEstimator p = new PiEstimator(10000);
-        if(args.length > 0) {
-            p = new PiEstimator(Integer.parseInt(args[0]));
+
+        try {
+            if(args.length > 0) {
+                p = new PiEstimator(Integer.parseInt(args[0]));
+            }
+        } catch(NumberFormatException nfe) {
+            System.out.println("Must enter a number. Default value of 10,000 entered.");
         }
+        
 
         System.out.println("start");
         int hits = 0;
 
 
         for(int i = 0; i < p.numDarts; i++) {
-            p.d = new Dart();
-
             System.out.println(p.throwDart() + (p.d.onBoard() ? "in" : "out"));
             if(p.d.onBoard()) {
                 hits++;
@@ -31,10 +35,10 @@ public class PiEstimator {
         System.out.println("Darts: " + p.numDarts + " Hits: " + hits + " Pi estimate: " + (double)(4*(double)hits)/(double)p.numDarts);
         System.out.println(hits + " darts thrown within the circle.");
 
-
     }
 
     public String throwDart() {
-        return d.x() + " " + d.y() + " ";
+        this.d = new Dart();
+        return this.d.x() + " " + this.d.y() + " ";
     }
 }
