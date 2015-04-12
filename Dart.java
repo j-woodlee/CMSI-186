2511b2557ba07c48d925eab6a92649a61b910e24
j-woodlee <jwoodlee@lion.lmu.edu>
2015-04-11 17:25:49 -0700

    Added new constructor to work with MonteCarlo

--- Dart.java
+++ Dart.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Dart {
     private double x; 
     private double y;
@@ -5,6 +7,18 @@ public class Dart {
     public Dart() {
         this.x = Math.random() * (Math.random() > .5 ? +1 : -1);
         this.y = Math.random() * (Math.random() > .5 ? +1 : -1);
+    }
+
+    public Dart(MonteCarloIntegrator mci) {//http://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values
+        Random r = new Random();
+        this.x = mci.getLowerRange() + (mci.getUpperRange() - mci.getLowerRange()) * r.nextDouble();
+        this.y = 9.0;
+    }
+
+    public static void main(String[] args) {
+        MonteCarloIntegrator mci = new MonteCarloIntegrator(10000, 4.5,6.5,new double[10]);
+        Dart d = new Dart(mci);
+        System.out.println(d);
     }
 
     public boolean onBoard() {
@@ -17,5 +31,10 @@ public class Dart {
 
     public double y() {
         return y;
+    }
+
+    @Override
+    public String toString() {
+        return x + ", " + y;
     }
 }
