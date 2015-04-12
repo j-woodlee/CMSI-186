229f7e87b61904daf25a5e332f8a3312529463b3
j-woodlee <jwoodlee@lion.lmu.edu>
2015-04-11 22:00:59 -0700

    Will Check if Dart is under a function

--- Dart.java
+++ Dart.java
@@ -11,18 +11,33 @@ public class Dart {
 
     public Dart(MonteCarloIntegrator mci) {//http://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values
         Random r = new Random();
-        this.x = mci.getLowerRange() + (mci.getUpperRange() - mci.getLowerRange()) * r.nextDouble();
-        this.y = 9.0;
+        double xMax = mci.getUpperRange();
+        double xMin = mci.getLowerRange();
+        double yMax = mci.findMax() + (0.1 * mci.findMax());
+        double yMin = mci.findMin() - (0.1 * mci.findMin());
+        this.x = xMin + (xMax - xMin) * r.nextDouble();
+        this.y = yMin + (yMax - yMin) * r.nextDouble();
     }
 
     public static void main(String[] args) {
-        MonteCarloIntegrator mci = new MonteCarloIntegrator(10000, 4.5,6.5,new double[10]);
+        MonteCarloIntegrator mci = new MonteCarloIntegrator(10000, 0.5, 1.0, new double[]{9.0, 8.0, 7.0, 6.0});
         Dart d = new Dart(mci);
         System.out.println(d);
     }
 
-    public boolean onBoard() {
+    public boolean onUnitCircle() {
         return Math.pow(this.x,2) + Math.pow(this.y,2) <= 1;
+    }
+
+    public boolean underFunction(MonteCarloIntegrator mci) {
+        if(mci.f(x) > 0 && y > 0 && mci.f(x) >= y) {
+            return true;
+        } else if(mci.f(x) < 0 && y < 0 && mci.f(x) <= y) {
+            return true;
+        } else if (mci.f(x) == 0 && y == 0){
+            return true;
+        }
+        return false;
     }
 
     public double x() {
